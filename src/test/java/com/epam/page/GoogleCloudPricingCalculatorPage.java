21cbd64dbaca35f41bee0734113531324d82a1b2
Arseniy Bakulmanov <backulmanov@yandex.ru>
2022-11-27 10:37:13 +0300

    Revert "Revert "load project""
    This reverts commit f6c2737f8fbb676cc6d996e43e3a61dfa6e58d2b.

---
+++ src/test/java/com/epam/page/GoogleCloudPricingCalculatorPage.java
@@ -0,0 +1,76 @@
+package com.epam.page;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class GoogleCloudPricingCalculatorPage extends AbstractPage {
+    private final Logger logger = LogManager.getRootLogger();
+    @FindBy(xpath = "//md-tab-item/div[@title='Compute Engine']")
+    private WebElement computeEngineButton;
+    @FindBy(id = "input_90")
+    private WebElement numberOfInstancesTextBox;
+    @FindBy(id = "select_value_label_82")
+    private WebElement operationSystemDropBox;
+    @FindBy(xpath = "//md-select[@placeholder='VM Class']")
+    private WebElement virtualMachineDropBox;
+    @FindBy(id = "select_value_label_85")
+    private WebElement machineSeriesDropBox;
+    @FindBy(xpath = "//md-select[@placeholder='Instance type']")
+    private WebElement machineParamDropBox;
+
+    public GoogleCloudPricingCalculatorPage(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void switchToAFrame() {
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(driver.findElement(By.xpath("//*[@id='cloud-site']/devsite-iframe/iframe")));
+        driver.switchTo().frame("myFrame");
+    }
+
+    public GoogleCloudPricingCalculatorPage activeComputeEngineSection() {
+        logger.info("Starting to filling up Cloud Form");
+        switchToAFrame();
+        getClickableElement(computeEngineButton).click();
+        return this;
+    }
+
+    public GoogleCloudPricingCalculatorPage setNumberOfInstances(String numberOfInstances) {
+        getClickableElement(numberOfInstancesTextBox).click();
+        numberOfInstancesTextBox.sendKeys(numberOfInstances);
+        return this;
+    }
+
+    public GoogleCloudPricingCalculatorPage setSoftware(String software) {
+        operationSystemDropBox.click();
+        WebElement softwareElement = driver.findElement(By.xpath("//md-option/div[contains(text(),'" + software + "')]"));
+        getClickableElement(softwareElement).click();
+        return this;
+
+    }
+
+    public GoogleCloudPricingCalculatorPage setProvisioningModel(String vmClass) {
+        driver.switchTo().defaultContent();
+        scrollDown(300);
+        switchToAFrame();
+        virtualMachineDropBox.click();
+        WebElement vmElement = driver.findElement(By.xpath("//md-option/div[contains(text(),'" + vmClass + "')]"));
+        getClickableElement(vmElement).click();
+        return this;
+    }
+
+    public GoogleCloudPricingCalculatorAdvancedSettingsPage setInstanceType(String machineSeries, String machineType) {
+        machineSeriesDropBox.click();
+        WebElement series = driver.findElement(By.xpath("//md-option/div[contains(text(),'" + machineSeries + "')]"));
+        getClickableElement(series).click();
+        machineParamDropBox.click();
+        WebElement type = driver.findElement(By.xpath("//md-option/div[contains(text(),'" + machineType + "')]"));
+        getClickableElement(type).click();
+        return new GoogleCloudPricingCalculatorAdvancedSettingsPage(driver);
+    }
+}
---
+++ src/test/java/com/epam/page/YopMailGeneratorPage.java
@@ -0,0 +1,59 @@
+package com.epam.page;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YopMailGeneratorPage extends AbstractPage {
+    private final Logger logger = LogManager.getRootLogger();
+    @FindBy(id = "egen")
+    private WebElement emailElement;
+    @FindBy(xpath = "//*[@onclick='egengo();']")
+    private WebElement checkEmailButton;
+    @FindBy(xpath = "//h2[text()]")
+    private WebElement mailSum;
+    @FindBy(id = "refresh")
+    private WebElement refreshButton;
+
+    public YopMailGeneratorPage(WebDriver yopDriver) {
+        super(yopDriver);
+        PageFactory.initElements(yopDriver, this);
+    }
+
+    public String getGeneratedEmailText() {
+        return emailElement.getText();
+    }
+
+    public String checkEmail() {
+        int count = 0;
+        checkEmailButton.click();
+        captchaCheck();
+        WebElement text = new WebDriverWait(driver, Duration.ofSeconds(FIVE_SECONDS_TIMEOUT))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='message']")));
+        while ((text.getAttribute("innerText").contains("Этот почтовый ящик пуст")) || count < 2) {
+            getClickableElement(refreshButton).click();
+            count++;
+        }
+        driver.switchTo().frame("ifmail");
+        return mailSum.getText();
+    }
+
+    private void captchaCheck() {
+        try {
+            new WebDriverWait(driver, Duration.ofSeconds(6))
+                    .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By
+                            .xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
+            getClickableElement(By
+                    .xpath("//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']")).click();
+            logger.warn("reCaptcha was caught");
+        } catch (org.openqa.selenium.NoSuchElementException | TimeoutException ignored) {
+
+        }
+    }
+}
